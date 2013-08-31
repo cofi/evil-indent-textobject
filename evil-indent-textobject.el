0725a179d7164b84f514d4efd4f2e9c6dad303c6
Michael Markert <markert.michael@googlemail.com>
2013-08-31 23:48:13 +0200

    Add helper functions to determine indent blocks.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

---
+++ evil-indent-textobject.el
@@ -0,0 +1,63 @@
+;;; evil-indent-textobject.el --- evil textobjects based on indentation
+
+;; Copyright (C) 2013 Michael Markert
+;; Author: Michael Markert <markert.michael@gmail.com>
+;; Created: 2013-08-31
+;; Version:
+;; Keywords: convenience evil
+;; URL: http://github.com/cofi/evil-indent-textobject
+;;
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'evil)
+
+(defun evil-indent--current-indentation ()
+  "Return the indentation of the current line.
+Moves point."
+  (buffer-substring-no-properties (point-at-bol)
+                                  (progn (back-to-indentation)
+                                         (point))))
+
+(defun evil-indent--same-indent-range (&optional point)
+  "Return the point at the begin and end of the text block with the same indentation.
+If `point' is supplied and non-nil it will return the begin and
+end of the block surrounding point."
+  (save-excursion
+    (when point
+      (goto-char point))
+    (let ((start (point))
+          (indent (evil-indent--current-indentation))
+          begin end)
+      (loop while (string= (evil-indent--current-indentation) indent)
+            do (progn
+                 (setq begin (point-at-bol))
+                 (forward-line -1)))
+      (goto-char start)
+      (loop while (string= (evil-indent--current-indentation) indent)
+            do (progn
+                 (setq end (point-at-eol))
+                 (forward-line 1)))
+      (list begin end))))
+
+(provide 'evil-indent-textobject)
+;;; evil-indent-textobject.el ends here
